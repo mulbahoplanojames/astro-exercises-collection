dd85a1c3fe8dee43ef63282170a7f8a9e1048198
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2025-02-18 10:42:25 +0200

    Updated the 📁, index.astro for a test on git submodule repo

--- astro-lesson1/.astro/content-modules.mjs
+++ astro-lesson1/.astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/posts/creating-a-successful-music-brand.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fcreating-a-successful-music-brand.mdx&astroContentModuleFlag=true")],
-["src/content/posts/collaboration-in-music-production.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fcollaboration-in-music-production.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/learnin-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Flearnin-astro.mdx&astroContentModuleFlag=true")],
-["src/content/articles/react-19-features.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Freact-19-features.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/react-19-features.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Freact-19-features.mdx&astroContentModuleFlag=true")],
+["src/content/posts/collaboration-in-music-production.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fcollaboration-in-music-production.mdx&astroContentModuleFlag=true")],
+["src/content/posts/creating-a-successful-music-brand.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fcreating-a-successful-music-brand.mdx&astroContentModuleFlag=true")]]);
 		
